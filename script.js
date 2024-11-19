dd67ae82a41a924e1c2800b393aae9079a4de4b3
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-20 00:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+async function Printmessage(){
+
+    const delay = parseInt(document.getElementById("delay").value,10);
+const text = document.getElementById("text").value;
+
+    if(!delay){
+        output.innerText = "Please enter delay"
+        return;
+    }
+        
+
+    if(isNaN(delay) || delay<0){
+        output.innerText= "Please enter Positive delay value";
+        return;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve , delay))
+    
+    output.innerText = text;
+
+}
+
+btn.addEventListener("click" , Printmessage);
+
+
